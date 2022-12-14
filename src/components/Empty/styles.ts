5083937b3861df215a3c6dadf8f01ffcd2fa21e6
Cristiano <cristianomorgante@hotmail.com>
2022-12-14 00:11:20 -0300

    Make Empty component responsive

--- src/components/Empty/styles.ts
+++ src/components/Empty/styles.ts
@@ -1,10 +1,11 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.section`
   ${({ theme }) => css`
     display: grid;
     place-items: center;
-    padding: ${theme.spacings.xxlarge};
+    padding: ${theme.spacings.xxlarge} 0;
     border-top: 1px solid ${theme.colors.gray400};
     border-radius: ${theme.border.radius};
   `}
@@ -22,10 +23,15 @@ export const Text = styled.p`
   ${({ theme }) => css`
     color: ${theme.colors.gray300};
     line-height: ${theme.spacings.medium};
+    font-size: ${theme.font.sizes.small};
 
     & span {
       font-weight: bold;
       display: block;
     }
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.medium};
+    `}
   `}
 `
