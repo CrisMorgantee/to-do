79dbf59e5dd048797c4446d8bb5bbb4ce1691ff5
Cristiano <cristianomorgante@hotmail.com>
2022-12-21 02:12:44 -0300

    Add task create to form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,44 @@
 import { AddCircleOutline as ButtonIcon } from '@styled-icons/material-outlined'
+import { useState } from 'react'
 import Button from '../Button'
+import { TaskProps } from '../Task'
 import * as S from './styles'
 
-const Form = () => {
+type FormProps = {
+  initialValue?: string
+  setTasks?: React.Dispatch<React.SetStateAction<TaskProps[]>>
+  tasks?: TaskProps[] | undefined
+}
+
+const Form = ({ tasks, setTasks, initialValue = '' }: FormProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.currentTarget.value
+    setValue(e.currentTarget.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    const task = { title: value }
+
+    setTasks!([...tasks!, task])
+    localStorage.setItem('tasks', JSON.stringify([...tasks!, task]))
+    setValue('')
+  }
+
   return (
-    <S.Wrapper>
-      <S.Input placeholder="Adicione uma nova tarefa" />
-      <Button icon={<ButtonIcon />}>Criar</Button>
+    <S.Wrapper onSubmit={handleSubmit}>
+      <S.Input
+        name="task"
+        placeholder="Adicione uma nova tarefa"
+        onChange={onChange}
+        value={value}
+      />
+      <Button type="submit" icon={<ButtonIcon />}>
+        Criar
+      </Button>
     </S.Wrapper>
   )
 }
